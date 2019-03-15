6d88ef104b508d72319351f3de160cec008e5bc5
xrxr19990625 <xur2@rpi.edu>
2019-03-15 04:04:29 -0400

    1. Deleted unused apis

--- api.py
+++ api.py
@@ -9,20 +9,6 @@ def get_form_data():
     return render_template('add_question.html')
 
 
-@app.route('/style', methods=['GET'])
-def style():
-    stl = open('stylesheet.css')
-    res = stl.read()
-    return res
-
-
-@app.route('/add_q_f', methods=['GET'])
-def js_f():
-    js = open('add_question_functions.js')
-    res = js.read()
-    return res
-
-
 @app.route('/add', methods=['POST', "GET"])
 def add():
     q_code = request.form['q-code']

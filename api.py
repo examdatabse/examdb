35cb59df43acb2ad4fbebab72b776936f2a7949e
xrxr19990625 <xur2@rpi.edu>
2019-03-15 04:03:15 -0400

    1. Organized templates files and static files 2. fixed css and js not found 3. the server can accept data now

--- api.py
+++ api.py
@@ -4,13 +4,29 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['POST', 'GET'])
 def get_form_data():
-    page = open('add_question.html', encoding='utf-8')
-    res = page.read()
+    # page = open('add_question.html', encoding='utf-8')
+    # res = page.read()
+    return render_template('add_question.html')
+
+
+@app.route('/style', methods=['GET'])
+def style():
+    stl = open('stylesheet.css')
+    res = stl.read()
+    return res
+
+
+@app.route('/add_q_f', methods=['GET'])
+def js_f():
+    js = open('add_question_functions.js')
+    res = js.read()
     return res
 
 
 @app.route('/add', methods=['POST', "GET"])
 def add():
+    q_code = request.form['q-code']
+    print(q_code)
     return "Added"
 
 

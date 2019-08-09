c2d769d8a86f89a0932a8846f0b107888cfae0d2
xrxr19990625 <xur2@rpi.edu>
2019-08-09 16:00:43 -0400

    1. Server can accept images now TODO: 1. choices and choice_images can only accept 1 image currently

--- app.py
+++ app.py
@@ -3,84 +3,131 @@ import time
 from examdb.databse import Problems
 
 
-def query_question(id):
-    query = Problems.select().where(Problems.id == id)
-    if len(query) == 0:
-        return False, ''
-    else:
+import time
+import pickle
+from examdb.databse import Problems
+from examdb.cache import Cache
+
+
+class QueryResult:
+    _id = 0
+    _serial_code = ''
+    _tags = []
+    _question = ''
+    _image = ''
+    _answer = ''
+    _ans_images = {}
+    _ans_choices = {}
+    _time_stamp = 0
+
+    def __init__(self, id, serial, tags, question, img, ans, ans_imgs, ans_choices, time):
+        self._id = id
+        self._serial_code = serial
+        self._tags = tags
+        self._question = question
+        self._image = img
+        self._answer = ans
+        self._ans_images = ans_imgs
+        self._ans_choices = ans_choices
+        self._time_stamp = time
+        pass
+
+
+class QueryQuestion:
+
+    _id_cache = Cache()
+    _tags_cache = Cache()
+
+    def __init__(self):
+        pass
+
+    def query(self, method, parameters):
+        if method == '0':
+            self.query_by_id(parameters)
+        elif method == '1':
+            self.query_by_tags(parameters)
+        else:
+            self.query_by_keywords(parameters)
+
+    def query_by_id(self, id):
+        if self._id_cache.contain(id):
+            return
+        query = Problems.select().where(Problems.id == id)
+        if len(query) == 0:
+            return False, ''
+        else:
+            for q in query:
+                return True, {
+                    'id': q.id,
+                    'serial': q.serial_code,
+                    'tags': q.tags,
+                    'testlet': q.question,
+                    'permission': q.permission,
+                    'question_image': q.question_image,
+                    'answer': q.answer,
+                    'ans_image1': q.answer_image_a,
+                    'ans_image2': q.answer_image_b,
+                    'ans_image3': q.answer_image_c,
+                    'ans_image4': q.answer_image_d,
+                    'ans_image5': q.answer_image_e,
+                    'choice1': q.choice_1,
+                    'choice2': q.choice_2,
+                    'choice3': q.choice_3,
+                    'choice4': q.choice_4,
+                    'choice5': q.choice_5,
+                    'time': time
+                }
+
+    def query_by_tags(self, tag):
+        query = Problems.select().where(True)
+        result = list()
         for q in query:
-            return True, {
-                'id': q.id,
-                'serial': q.serial_code,
-                'tags': q.tags,
-                'testlet': q.question,
-                'question_image': q.question_image,
-                'answer': q.answer,
-                'ans_image1': q.answer_image_a,
-                'ans_image2': q.answer_image_b,
-                'ans_image3': q.answer_image_c,
-                'ans_image4': q.answer_image_d,
-                'ans_image5': q.answer_image_e,
-                'choice1': q.choice_1,
-                'choice2': q.choice_2,
-                'choice3': q.choice_3,
-                'choice4': q.choice_4,
-                'choice5': q.choice_5,
-                'time': time
-            }
+            tags = set(q.tags.split('&'))
+            if tag in tags:
+                result.append({
+                    'id': q.id,
+                    'serial': q.serial_code,
+                    'tags': q.tags,
+                    'testlet': q.question,
+                    'permission': q.permission,
+                    'question_image': q.question_image,
+                    'answer': q.answer,
+                    'ans_image1': q.answer_image_a,
+                    'ans_image2': q.answer_image_b,
+                    'ans_image3': q.answer_image_c,
+                    'ans_image4': q.answer_image_d,
+                    'ans_image5': q.answer_image_e,
+                    'choice1': q.choice_1,
+                    'choice2': q.choice_2,
+                    'choice3': q.choice_3,
+                    'choice4': q.choice_4,
+                    'choice5': q.choice_5,
+                    'time': time
+                })
+
+    def query_by_keywords(self, tags):
+        pass
 
 
 def add_questions(form, files):
-    print(form)
-    print(files)
-    q_code = form['q-code']
-    permission = form['permission']
-    question = form['question']
-    choice1 = form['choice1']
-    choice2 = form['choice2']
-    choice3 = form['choice3']
-    choice4 = form['choice4']
-    choice5 = form['choice5']
-    choice1_img = files.get('choice_image1')
-    choice2_img = files.get('choice_image2')
-    choice3_img = files.get('choice_image3')
-    choice4_img = files.get('choice_image4')
-    choice5_img = files.get('choice_image5')
-    print(type(choice1))
-    answerfile = files.get('answer')
-    question_img = files.get('question_image')
-    if choice1 == '' or choice2 == '' or choice3 == '' or choice4 == '' or choice5 == '' or q_code == '' \
-            or question == '' or permission == '':
-        raise Exception('Required fields empty')
-    if choice1_img is None:
-        choice1_img = bytes('', 'utf-8')
-    else:
-        choice1_img = choice1_img.read()
-    if choice2_img is None:
-        choice2_img = bytes('', 'utf-8')
-    else:
-        choice2_img = choice2_img.read()
-    if choice3_img is None:
-        choice3_img = bytes('', 'utf-8')
-    else:
-        choice3_img = choice3_img.read()
-    if choice4_img is None:
-        choice4_img = bytes('', 'utf-8')
-    else:
-        choice4_img = choice4_img.read()
-    if choice5_img is None:
-        choice5_img = bytes('', 'utf-8')
-    else:
-        choice5_img = choice5_img.read()
-    if answerfile is None:
-        answerfile = bytes('', 'utf-8')
-    else:
-        answerfile = answerfile.read()
-    if question_img is None:
-        question_img = bytes('', 'utf-8')
-    else:
-        question_img = question_img.read()
-    Problems.create(serial_code=q_code, permission=permission, question=question, choice1=choice1,
-                    choice2=choice2, choice3=choice3, choice4=choice4, choice5=choice5, timestamp=time.time(),
-                    answer_image_a=choice1_img, answer_image_b=choice2_img, answer_image_c=choice3_img,
-                    answer_image_d=choice4_img, answer_image_e=choice5_img, question_image=question_img)
+    choices = form.getlist('choice')
+    choice_images = files.getlist('choice_image')
+    for i in range(5 - len(choices)):
+        choices.append('')
+    for i in range(5 - len(choice_images)):
+        choice_images.append('')
+    for i in range(5):
+        choice_images[i] = pickle.dumps(choice_images[i])
+
+    Problems.create(serial_code=form['q-code'], permission=form['permission'], question=form['question'],
+                    choice_1=choices[0], choice_2=choices[1], choice_3=choices[2], choice_4=choices[3], choice_5=
+                    choices[4], answer_image_a=choice_images[0], answer_image_b=choice_images[1], answer_image_c=
+                    choice_images[2], answer_image_d=choice_images[3], answer_image_e=choice_images[4],
+                    time=time.time()
+                    )
+    pass
+
+
+def bulk_add(form, files):
+
+    pass
---
+++ docs.py
@@ -0,0 +1,105 @@
+import pypandoc
+from bs4 import BeautifulSoup
+
+
+def parse_doc(docname, offset):
+    output = pypandoc.convert_file(docname, 'html', extra_args=['--extract-media=static/{}'.format(offset)])
+    soup = BeautifulSoup(output, "html.parser")
+    all_questions = soup.find_all(["p", "table"])
+    all_questions = list(all_questions)
+    i = 0
+    question_info = dict()
+    curr_code = ""
+    while i < len(all_questions):
+        j = all_questions[i]
+        portion = j.text[1:len(j.text)].split(":")
+        if portion[0] == "QCode":
+            portion[1] = portion[1].replace("{", "")
+            portion[1] = portion[1].replace("}", "")
+            curr_code = portion[1]
+            question_info[curr_code] = dict()
+            # print("Question Number:", portion[1])
+
+        if portion[0] == "Serial":
+            portion[1] = portion[1].replace("{", "")
+            portion[1] = portion[1].replace("}", "")
+            question_info[curr_code]["serial"] = portion[1]
+            # print("Serial Number:", portion[1])
+
+        if portion[0] == "Permission":
+            portion[1] = portion[1].replace("{", "")
+            portion[1] = portion[1].replace("}", "")
+            question_info[curr_code]["permission"] = portion[1]
+            # print("Permission:", portion[1])
+
+        if portion[0] == "Ans":
+            portion[1] = portion[1].replace("{", "")
+            portion[1] = portion[1].replace("}", "")
+            question_info[curr_code]["ans"] = portion[1]
+            # print("Answer:", portion[1])
+
+        if portion[0] == "Question":
+            question_body = str(j).replace("@Question:", "")
+            if question_body.find("{") != -1 and question_body.find("}") == -1:
+                long_question = ""
+                while True:
+                    content = str(all_questions[i])
+                    long_question += content + "\n"
+                    if content.find("}") != -1:
+                        break
+                    i += 1
+                question_body = long_question
+            question_body = question_body.replace("@Question:", "")
+            question_body = question_body.replace("{", "")
+            question_body = question_body.replace("}", "")
+            question_info[curr_code]["qbody"] = question_body
+            # print("Question Body:\n", question_body)
+
+        if portion[0] == "Choice1":
+            choice = str(j).replace("@Choice1:", "")
+            choice = choice.replace("{", "")
+            choice = choice.replace("}", "")
+            question_info[curr_code]["c1"] = choice
+            # print("Choice1", choice)
+
+        if portion[0] == "Choice2":
+            choice = str(j).replace("@Choice2:", "")
+            choice = choice.replace("{", "")
+            choice = choice.replace("}", "")
+            question_info[curr_code]["c2"] = choice
+            # print("Choice2", choice)
+
+        if portion[0] == "Choice3":
+            choice = str(j).replace("@Choice3:", "")
+            choice = choice.replace("{", "")
+            choice = choice.replace("}", "")
+            question_info[curr_code]["c3"] = choice
+            # print("Choice3", choice)
+
+        if portion[0] == "Choice4":
+            choice = str(j).replace("@Choice4:", "")
+            choice = choice.replace("{", "")
+            choice = choice.replace("}", "")
+            question_info[curr_code]["c4"] = choice
+            # print("Choice4", choice)
+
+        if portion[0] == "Choice5":
+            choice = str(j).replace("@Choice5:", "")
+            choice = choice.replace("{", "")
+            choice = choice.replace("}", "")
+            question_info[curr_code]["c5"] = choice
+            # print("Choice5", choice)
+
+        # print(j)
+        i += 1
+
+    return question_info, output
+
+
+if __name__ == '__main__':
+    print(parse_doc("test.docx", 3))
+
+    # with open("test1.html", 'r') as f:
+    #     for i in f:
+    #         print(i)
+
